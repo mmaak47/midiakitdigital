7d6d5bcf2927eb4335da3b742df5f33e98c60ab7
Maite Doin <maitedoin@gmail.com>
2026-04-12 21:06:41 -0300

    fix: fixando

--- backend/server.js
+++ backend/server.js
@@ -277,9 +277,9 @@ app.use((req, res, next) => {
   const csp = [
     "default-src 'self'",
     "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-    "style-src 'self' 'unsafe-inline'",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "img-src 'self' data: blob: https:",
-    "font-src 'self' data:",
+    "font-src 'self' data: https://fonts.gstatic.com",
     "connect-src 'self' https:",
     "worker-src 'self' blob:",
     "frame-ancestors 'none'",
--- frontend/src/pages/CampaignPlanner.jsx
+++ frontend/src/pages/CampaignPlanner.jsx
@@ -1573,7 +1573,6 @@ export default function CampaignPlanner() {
                       </div>
                     );
                   })}
-                  ))}
                 </div>
               </div>
             )}
